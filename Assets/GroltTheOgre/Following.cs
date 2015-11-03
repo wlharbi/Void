ce084faae7184265683df34c9f5b96c61cef7cc8
Anas <anas2006101@gmail.com>
2015-11-03 23:54:38 +0300

    Monster Animation
    Importing and starting at idle state

---
+++ Assets/GroltTheOgre/Following.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Following : MonoBehaviour {
+
+    var leader : Transform;
+var follower : Transform;
+var speed : float =5; // The speed of the follower
+ 
+function Update()
+    {
+        follower.LookAt(leader);
+        follower.Translate(speed * Vector3.forward * Time.deltaTime);
+    }
+
+}

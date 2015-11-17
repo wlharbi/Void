ee45d05d0ca915a313d83806020f429bd184230c
aljathmi <x.2c@hotmail.com>
2015-11-17 23:44:21 +0300

    puzzle 40%

---
+++ Assets/PuzzleControl01.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleControl01 : MonoBehaviour {
+
+    private bool isFinishedCorrectly = false;
+    private bool isFailed = false;
+
+
+    //The player can go from blue to green, green to red, and red to blue.
+    //Any other moves will cause the player to fail and restart the puzzle.
+    private string firstColor = "Blue";
+    private string secondColor = "Green";
+    private string thirdColor = "Red";
+
+    private string trapColor = "Black";
+
+
+    private string lastColor = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public bool checkValidity(string destinationColor)
+    {
+        print("CheckValidity is Called");
+        if (lastColor == null)
+        {
+            if (destinationColor != firstColor) return false;
+            lastColor = destinationColor;
+            return true;
+        }
+        else if (lastColor == firstColor)
+        {
+            if (destinationColor == firstColor || destinationColor == thirdColor)
+            {
+                fail();
+                return false;
+            }
+            else
+            {
+                correctMove(destinationColor);
+                return true;
+            }
+        }
+        else if (lastColor == secondColor)
+        {
+            if (destinationColor == secondColor || destinationColor == firstColor)
+            {
+                fail();
+                return false;
+            }
+            else
+            {
+                correctMove(destinationColor);
+                return true;
+            }
+        }
+        else if (lastColor == thirdColor)
+        {
+            if (destinationColor == thirdColor || destinationColor == secondColor)
+            {
+                fail();
+                return false;
+            }
+            else
+            {
+                correctMove(destinationColor);
+                return true;
+            }
+        }
+        else
+            return false;
+    }
+
+    public void fail()
+    {
+        lastColor = null;
+    }
+    public void correctMove(string destinationColor)
+    {
+        lastColor = destinationColor;
+    }
+}
---
+++ Assets/reportOnCollison.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reportOnCollison : MonoBehaviour 
+{
+    public Renderer rend;
+
+    private bool isValidMove;
+    string[] colorName;
+    GameObject puzzleController;
+    private Material oldMaterial;
+    private Material newMaterial = Resources.Load("Decrepit Dungeon/Materials/CorrectMove", typeof(Material)) as Material;
+
+    void start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        oldMaterial = rend.material;
+        puzzleController = gameObject.transform.parent.gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        colorName = gameObject.transform.name.Split(' ');
+        
+        isValidMove = puzzleController.checkValidity(colorName[0]);
+        print(colorName[0] + " is " + isValidMove);
+        reaction(isValidMove);
+    }
+
+    public void reaction(bool isValid)
+    {
+        rend.sharedMaterial = newMaterial;
+    }
+}
